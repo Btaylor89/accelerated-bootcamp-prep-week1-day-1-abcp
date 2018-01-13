20385f39cdcfd8123e8b4309c21cde9e43b61497
Britni Chavis <brit.chavis@gmail.com>
2018-01-13 17:28:07 +0000

    automatically backed up by learn

--- variables.js
+++ variables.js
@@ -8,6 +8,8 @@ console.log(personality)
 personality = "fabulous";
 console.log(personality);
 
+var beyoncesMoney = "500 + 750 + 3000 + 5100 + 12980 + 9087 + 50000";
+console.log(beyoncesMoney);
 // Your Turn!
 // change the value of the `personality` variable to something that describes you
 // and console.log the new value
